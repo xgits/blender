f90aeb0152a4b2309ae4a97503c8fa84acdb4045
Joseph Eagar <joeedh@gmail.com>
2022-06-02 16:47:03 -0700

    temp-pbvh-split: Remove debug macro

--- source/blender/blenkernel/BKE_pbvh.h
+++ source/blender/blenkernel/BKE_pbvh.h
@@ -594,14 +594,3 @@ bool BKE_pbvh_draw_cache_invalid(const PBVH *pbvh);
 #ifdef __cplusplus
 }
 #endif
-
-/* Disable optimization for a function (for debugging use only!)*/
-#ifdef __clang__
-#  define ATTR_NO_OPT __attribute__((optnone))
-#elif defined(_MSC_VER)
-#  define ATTR_NO_OPT __pragma(optimize("", off))
-#elif defined(__GNUC__)
-#  define ATTR_NO_OPT __attribute__((optimize("O0")))
-#else
-#  define ATTR_NO_OPT
-#endif
